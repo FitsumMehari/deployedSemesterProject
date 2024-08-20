fdb38ded1f984258872f210e9ff4bec9d48ae09e
FitsumMehari <m.fitsummehari@gmail.com>
2024-08-20 13:38:10 +0300

    add updated build that contains: spinner, timer, and styling on buttons on footer and question navigation

---
+++ browser/chunk-N25JQAON.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-FHRPQUTC.js";import"./chunk-LWLIZU7P.js";export{a as UserModule};
---
+++ browser/chunk-OUHS4NNS.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-YT6SPZ2C.js";import"./chunk-LWLIZU7P.js";export{a as AdminModule};
