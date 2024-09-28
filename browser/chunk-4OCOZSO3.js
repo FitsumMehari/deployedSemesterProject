2e76a91b2defc4b66e49ac29080d3da097fa3721
FitsumMehari <m.fitsummehari@gmail.com>
2024-09-28 11:08:36 +0300

    add translation, make the download button actually download, add tutorials link on navbar

---
+++ browser/chunk-4OCOZSO3.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-5YUPE35I.js";import"./chunk-TUPWRAQV.js";export{a as AdminModule};
---
+++ browser/chunk-UN4DKKNB.js
@@ -0,0 +1,1 @@
+import{f as a}from"./chunk-BHGVST4Z.js";import"./chunk-TUPWRAQV.js";export{a as UserModule};
