2d97643aee95b01dab85dfa76b4dcad040efb6d8
FitsumMehari <m.fitsummehari@gmail.com>
2024-10-29 12:04:02 +0300

    fix spelling error

--- browser/chunk-H2YNS3XL.js
+++
@@ -1,1 +0,0 @@
-import{f as a}from"./chunk-F4HB4GH4.js";import"./chunk-QQ65KV5H.js";export{a as UserModule};
---
+++ browser/chunk-LS2GSFYQ.js
@@ -0,0 +1,1 @@
+import{f as a}from"./chunk-JSATNKNG.js";import"./chunk-QQ65KV5H.js";export{a as UserModule};
