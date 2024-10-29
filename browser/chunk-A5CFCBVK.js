92230bcff9b62e8ec41d3f4e302b8249a54bf64d
FitsumMehari <m.fitsummehari@gmail.com>
2024-10-29 11:57:45 +0300

    bug fixes on inputs

--- browser/chunk-A5CFCBVK.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-2VKGSVWW.js";import"./chunk-KJLX7J6U.js";export{a as AdminModule};
---
+++ browser/chunk-WTAZDXUW.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-F2B5PBXP.js";import"./chunk-QQ65KV5H.js";export{a as AdminModule};
