9a393689773bd44947c1cd3621991de2a5196130
FitsumMehari <m.fitsummehari@gmail.com>
2024-10-29 11:35:53 +0300

    fix footer links and spelling errors

---
+++ browser/chunk-55ZFR7AY.js
@@ -0,0 +1,1 @@
+import{f as a}from"./chunk-RLV5VOZ6.js";import"./chunk-KJLX7J6U.js";export{a as UserModule};
--- browser/chunk-GU264MK2.js
+++
@@ -1,1 +0,0 @@
-import{f as a}from"./chunk-OSGPLPXC.js";import"./chunk-KJLX7J6U.js";export{a as UserModule};
